2b8fc07b31a610236fa8b5af627871a6a9b1720e
Meyyappan <meyyappan721@gmail.com>
2024-09-03 21:42:58 +0530

    angular 18 - upto routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './common/about/about.component';
+import { ContactComponent } from './common/contact/contact.component';
+import { SupportComponent } from './common/support/support.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'support',
+    component: SupportComponent,
+  },
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { Observable, of } from 'rxjs';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { ReversePipe } from '../custom/reverse.pipe';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatCheckbox,
+    CommonModule,
+    ReversePipe,
+    FormsModule,
+    RouterOutlet,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  title = 'myapp';
+  subtitle = 'SUB-TITLE';
+  today = new Date();
+  price = 1234.56;
+  percentage = 0.25;
+  user = { name: 'John', age: 30 };
+  text = 'Angular';
+  numbers = [1, 2, 3, 4, 5];
+  observable$: Observable<string> = of('Hello from Observable!');
+  isDisabled: boolean = true;
+  name: string = '';
+  toggleDisabled() {
+    this.isDisabled = !this.isDisabled;
+  }
+  updateTitle(event: any) {
+    this.title = event.target.value;
+  }
+}
