59609edc0a687736b9bb1b0577b626c35525c0d1
Meyyappan <meyyappan721@gmail.com>
2024-09-16 09:22:18 +0530

    control flow templates

--- src/app/common/about/about.component.ts
+++ src/app/common/about/about.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CounterbuttonComponent } from '../counterbutton/counterbutton.component';
 import { CounterdisplayComponent } from '../counterdisplay/counterdisplay.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CounterbuttonComponent, CounterdisplayComponent],
+  imports: [CounterbuttonComponent, CounterdisplayComponent, CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
 export class AboutComponent implements OnInit {
   id: any;
+  countries = ['United States', 'Canada', 'India', 'UK'];
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
